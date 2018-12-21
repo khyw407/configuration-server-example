de03945eb7839f40199890307a90e32f7498e733
Hyunwoo Kim <khyw407@gmail.com>
2018-12-21 14:24:26 +0900

    add rabbitmq broker

--- src/main/java/com/test/springboot/ConfigServerApplication.java
+++ src/main/java/com/test/springboot/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,5 +12,10 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+	
+	@Bean
+	public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
+		return new GithubPropertyPathNotificationExtractor();
 	}
 }
