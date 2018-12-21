d55726cad6b878babf6c9e126e5ee5fb5d59e7ee
Hyunwoo Kim <khyw407@gmail.com>
2018-12-21 14:40:00 +0900

    rollback monitoring config

--- src/main/java/com/test/springboot/ConfigServerApplication.java
+++ src/main/java/com/test/springboot/ConfigServerApplication.java
@@ -2,9 +2,7 @@ package com.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.monitor.GithubPropertyPathNotificationExtractor;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,10 +10,5 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
-	}
-	
-	@Bean
-	public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
-		return new GithubPropertyPathNotificationExtractor();
 	}
 }
